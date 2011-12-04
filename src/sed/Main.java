48437e79086c73a4b14b35378a8f3b01163097a5
cn <cn@mycrobase.de>
2011-12-05 00:49:30 +0100

    New SkyAppState encapsulates SkyDome behavior
    Nice concept of jME for factoring out independent pieces of code that can be thought of a mixin of functionality for the final scene.

--- src/sed/Main.java
+++ src/sed/Main.java
@@ -1,18 +1,16 @@
 package sed;
 
-import sed.sky.SkyBoxTexture;
-import sed.sky.SkyDome;
-import sed.sky.SkyGradient;
+import ssim.sim.SimClock;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
-import com.jme3.material.RenderState.FaceCullMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.Spatial.CullHint;
 import com.jme3.scene.shape.Box;
-import com.jme3.util.BufferUtils;
 
 public class Main extends SimpleApplication {
 	
@@ -24,14 +22,22 @@ public class Main extends SimpleApplication {
 	
 	// TODO: Key z -> y
 	
-	private float time_sky = 0;
-	private Material s_mat;
-	private SkyBoxTexture sky_box_texture;
+	private float time = 0;
+	public SimClock clock;
 	
 	@Override
 	public void simpleInitApp() {
+		clock = SimClock.createClock(11.00f);
+		assert clock != null : "SimClock init failed - wrong parameters!";
+		
 		flyCam.setMoveSpeed(10 * 6);
 //		flyCam.setDragToRotate(true);
+		
+		Node skyNode = new Node("SkyNode");
+		skyNode.setCullHint(CullHint.Never);
+		rootNode.attachChild(skyNode);
+		
+		stateManager.attach(new SkyAppState());
 		
 		Box b = new Box(Vector3f.ZERO, 1, 1, 1);
 		Geometry geom = new Geometry("Box", b);
@@ -40,34 +46,32 @@ public class Main extends SimpleApplication {
 		geom.setMaterial(mat);
 //		rootNode.attachChild(geom);
 		
-		SkyDome s = new SkyDome();
-//		Box s = new Box(Vector3f.ZERO, 1, 1, 1);
-		Geometry s_geom = new Geometry("SkyDome", s);
-		s_mat = new Material(assetManager, "Shaders/Sky.j3md");
-//		s_mat.getAdditionalRenderState().setWireframe(true);
-		s_mat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
-//		s_mat.setColor("Color", ColorRGBA.Gray);
-		SkyGradient sg = new SkyGradient();
-		sg.setTurbidity(2f);
-		sg.updateSunPosition(12.00f, 180, 36.4f, (int)(11.8f/15f), 11.8f);
-		sky_box_texture = new SkyBoxTexture(sg);
-		sky_box_texture.update();
-		s_mat.setTexture("SkyBox", sky_box_texture);
-		s_geom.setMaterial(s_mat);
-		rootNode.attachChild(s_geom);
-		
-		rootNode.setCullHint(CullHint.Never);
-		
+		printSceneGraph(rootNode);
 	}
 	
 	@Override
-	public void simpleUpdate(float tpf) {		
-		if(time_sky > 2) {
-//			sky_box_texture.update();
-//			s_mat.setTexture("SkyBox", SkyBoxTexture.genRandomSkyBox(ColorRGBA.randomColor()));
-			time_sky = 0;
+	public void simpleUpdate(float tpf) {
+		if(time > 2) {
+			time = 0;
 		}
 		
-		time_sky += tpf;
+		time += tpf;
+		clock.step(tpf);
+	}
+	
+	private static void printSceneGraph(Node root) {
+		printSceneGraph(root, 0);
+	}
+	
+	private static void printSceneGraph(Spatial root, int indent) {
+		String s = "";
+		for(int i = 0; i < indent; i++) {
+			s += "  ";
+		}
+		System.out.println(s+root.toString());
+		if(!(root instanceof Node)) return;
+		for(Spatial c : ((Node)root).getChildren()) {
+			printSceneGraph(c, indent+1);
+		}
 	}
 }
