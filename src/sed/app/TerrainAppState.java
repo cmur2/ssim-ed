2758dabc3e14118bcab10704be6dfc05e711e6bc
cn <cn@mycrobase.de>
2012-03-11 01:03:48 +0100

    Add LOD with correct patch size
    --HG--
    branch : feature-terrain

--- src/sed/app/TerrainAppState.java
+++ src/sed/app/TerrainAppState.java
@@ -1,5 +1,7 @@
 package sed.app;
 
+import java.util.Arrays;
+
 import sed.MapLoader;
 import sed.terrain.BinaryMapTileLoader;
 
@@ -12,6 +14,7 @@ import com.jme3.math.Vector3f;
 import com.jme3.terrain.geomipmap.TerrainGrid;
 import com.jme3.terrain.geomipmap.TerrainGridTileLoader;
 import com.jme3.terrain.geomipmap.TerrainLodControl;
+import com.jme3.terrain.geomipmap.lodcalc.DistanceLodCalculator;
 
 public class TerrainAppState extends BasicAppState {
     
@@ -31,18 +34,22 @@ public class TerrainAppState extends BasicAppState {
         AssetKey<MapLoader.Map> mapKey = new AssetKey<MapLoader.Map>("maps/"+getApp().getMission().getMapFile());
         MapLoader.Map map = getApp().getAssetManager().loadAsset(mapKey);
         //System.out.println(map);
-                
+        
         Material mat = new Material(getApp().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Brown);
         
         TerrainGridTileLoader loader = new BinaryMapTileLoader(map);
         
-        TerrainGrid grid = new TerrainGrid("TerrainGrid", 65, 257, loader);
+        final int patchSize = 17;
+        TerrainGrid grid = new TerrainGrid("TerrainGrid", patchSize, 257, loader);
         grid.setMaterial(mat);
         grid.setLocalTranslation(0, 0, 0);
         grid.setLocalScale(1f, 1f, 1f);
         
-        grid.addControl(new TerrainLodControl(grid, getApp().getCamera()));
+        TerrainLodControl control = new TerrainLodControl(grid, Arrays.asList(getApp().getCamera()));
+        control.setLodCalculator(new DistanceLodCalculator(patchSize, 2.7f));
+        grid.addControl(control);
+        
         //grid.initialize(getApp().getCamera().getLocation());
         grid.initialize(new Vector3f());
 
