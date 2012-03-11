f509398893bcd05233a6134cdc087c5c9531090f
cn <cn@mycrobase.de>
2012-03-11 13:38:34 +0100

    Add notes for WindInterpolator

--- src/sed/weather/WindInterpolator.java
+++ src/sed/weather/WindInterpolator.java
@@ -6,7 +6,7 @@ import com.jme3.math.FastMath;
  * Wind is modeled as an 3D vector always perpendicular to the Y axis
  * (straight up) so the Y component is always zero. This vector gets mainly
  * constructed from an angle denoting the direction where the wind originates
- * from. Example: "320 degrees" mens wind from the NW
+ * from. Example: "320 degrees" means wind from the NW
  * 
  * @author cn
  */
@@ -17,8 +17,13 @@ public class WindInterpolator implements WeatherInterpolator {
         float a = (Float) valueA;
         float b = (Float) valueB;
         if(Math.abs(a-b) <= 180) {
+            // if difference is smaller than 180 degrees (the shorter way from
+            // a to b is positive-wise) do linear interpolation without wrapping
             return FastMath.interpolateLinear(ratio, a, b);
         } else {
+            // here the shorter way will cross the 0/360 mark, so increase the
+            // lower number by 360 which should make them <180 away positive-wise
+            // than subtract 360 from the result if necessary
             if(a < b) a += 360; else b += 360;
             float t = FastMath.interpolateLinear(ratio, a, b);
             return t >= 360 ? t-360 : t;
