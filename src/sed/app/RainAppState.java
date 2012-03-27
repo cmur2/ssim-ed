dec8e9dd1e26d69262a5c296ce1a3c3867abe7b4
cn <cn@mycrobase.de>
2012-03-27 10:31:39 +0200

    Implement rain particle tiles and controller
    Now a quad grid of selectable size and step using same Mesh and Material data for all tiles can be build. The grid resides below a special Node that will be moved according to the Camera's location. This calculation is done per frame so no visible cluttering should occur.

--- src/sed/app/RainAppState.java
+++ src/sed/app/RainAppState.java
@@ -1,21 +1,31 @@
 package sed.app;
 
 import sed.sky.RainParticles;
+import sed.util.TempVars;
+import ssim.util.MathExt;
 
 import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial.CullHint;
 
 public class RainAppState extends BasicAppState {
     
-    private static final float UpdateInterval = 10f; // in seconds
+    private static final float UpdateInterval = 2f; // in seconds
+    
+    private static final float GridStep = 100f; // in m
+    private static final int NumGridTiles = 5; // should be odd
+   
+    private static final float RainLowerY = 0f; // in m
+    private static final float RainUpperY = 500f; // in m
     
     // exists only while AppState is attached
+    private Node rainNode;
     private RainParticles rain;
-    private Geometry geom;
     
     public RainAppState() {
         super(UpdateInterval);
@@ -25,27 +35,34 @@ public class RainAppState extends BasicAppState {
     public void initialize(AppStateManager stateManager, Application baseApp) {
         super.initialize(stateManager, baseApp);
         
-        // 200, 5.5f, new Color3f(0.4f, 0.4f, 0.5f)S
         rain = new RainParticles(200, 100f);
         rain.setDropLength(5.5f); // in m
         rain.setDropLengthVar(0.5f); // in m
         rain.setDropColor(new ColorRGBA(0.4f, 0.4f, 0.5f, 1.0f));
         rain.setDropColorVar(new ColorRGBA(0.1f, 0.1f, 0.1f, 0.0f));
-        rain.setDropVelocity(30f); // in m/s
-        rain.setDropVelocityVar(5f); // in m/s
-        rain.setMinY(0f);
-        rain.setMaxY(200f);
+        rain.setDropVelocity(90f); // in m/s
+        rain.setDropVelocityVar(15f); // in m/s
+        rain.setMinY( -50f);
+        rain.setMaxY(+200f);
+        rain.setInitY(+400f);
         rain.initFirstDrops();
         
-        geom = new Geometry("RainParticles", rain);
+        rainNode = new Node("RainNode");
+        rainNode.setCullHint(CullHint.Never);
         
-        Material mat = new Material(getApp().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setBoolean("VertexColor", true);
+        Material rainMat = new Material(getApp().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        rainMat.setBoolean("VertexColor", true);
         
-        geom.setMaterial(mat);
-        geom.setCullHint(CullHint.Never);
+        final int numGridTilesHalf = NumGridTiles/2;
+        for(int ix = -numGridTilesHalf; ix <= +numGridTilesHalf; ix++) {
+            for(int iz = -numGridTilesHalf; iz <= +numGridTilesHalf; iz++) {
+                Vector3f offset = new Vector3f(ix,0,iz);
+                offset.multLocal(GridStep);
+                rainNode.attachChild(buildRainTile(rain, rainMat, offset));
+            }
+        }
         
-        getApp().getRootNode().attachChild(geom);
+        getApp().getRootNode().attachChild(rainNode);
     }
     
     @Override
@@ -54,6 +71,18 @@ public class RainAppState extends BasicAppState {
         super.update(dt);
         
         rain.update(dt);
+        
+        TempVars vars = TempVars.get();
+        
+        Vector3f loc = vars.vect1.set(getApp().getCamera().getLocation());
+        Vector3f gridLoc = vars.vect2.set(
+            MathExt.floor(loc.x/GridStep)*GridStep,
+            MathExt.clamp(loc.y, RainLowerY-(rain.getMinY()), RainUpperY-(rain.getMaxY())),
+            MathExt.floor(loc.z/GridStep)*GridStep
+        );
+        rainNode.setLocalTranslation(gridLoc);
+        
+        vars.release();
     }
     
     @Override
@@ -64,9 +93,16 @@ public class RainAppState extends BasicAppState {
     public void cleanup() {
         super.cleanup();
         
-        getApp().getRootNode().detachChild(geom);
+        getApp().getRootNode().detachChild(rainNode);
         
         rain = null;
-        geom = null;
+        rainNode = null;
+    }
+    
+    private Geometry buildRainTile(RainParticles rain, Material mat, Vector3f offset) {
+        Geometry geom = new Geometry("RainParticles"+offset.toString(), rain);
+        geom.setMaterial(mat);
+        geom.setLocalTranslation(offset);
+        return geom;
     }
 }
