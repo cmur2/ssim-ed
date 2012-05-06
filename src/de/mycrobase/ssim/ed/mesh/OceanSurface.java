76868dbba616ecdfe654bb294096f0477f28622c
cn <cn@mycrobase.de>
2012-05-06 13:35:02 +0200

    Add ocean normals based on raw sim data
    This may cause problems when introducing motion deltas.
    
    --HG--
    extra : rebase_source : 7d0fce39cdf34746f401b0d696f71faf7a129a9e

--- src/de/mycrobase/ssim/ed/mesh/OceanSurface.java
+++ src/de/mycrobase/ssim/ed/mesh/OceanSurface.java
@@ -30,12 +30,14 @@ public class OceanSurface extends Mesh {
     
     // sim data
     private float accTime;
-    private Vector3f[][] vPositions;
     // fHold saves fixed calculations for later use
     private Vector3f[][] fHold;
     private Vector2f[][] mH0;
     
     // temporary data, discarded between frames
+    private Vector3f[][] vPositions;
+    private Vector3f[][] vNormals;
+    private Vector3f[][] fNormals;
     private Vector2f[][] c;
     
     private VertexBuffer positionVBO;
@@ -98,6 +100,7 @@ public class OceanSurface extends Mesh {
         for(int ix = 0; ix < numVertexX; ix++) {
             for(int iy = 0; iy < numVertexY; iy++) {
                 vPositions[ix][iy] = new Vector3f();
+                vNormals[ix][iy] = new Vector3f();
             }
         }
 
@@ -105,7 +108,8 @@ public class OceanSurface extends Mesh {
             for(int iy = 0; iy < numY; iy++) {
                 fHold[ix][iy] = new Vector3f();
                 mH0[ix][iy] = new Vector2f();
-                
+
+                fNormals[ix][iy] = new Vector3f();
                 c[ix][iy] = new Vector2f();
             }
         }
@@ -159,24 +163,35 @@ public class OceanSurface extends Mesh {
             }
         }
         
+        updateFaceNormals();
+        
         for(int ix = 0; ix < numX; ix++) {
+            int ixLeft = MathExt.wrapByMax(ix-1, numX-1);
             for(int iy = 0; iy < numY; iy++) {
+                int iyLeft = MathExt.wrapByMax(iy-1, numY-1);
+                
                 vPositions[ix][iy].x = (float) ix/numX * scaleX;
                 vPositions[ix][iy].y = c[ix][iy].x * waveHeightScale;
                 vPositions[ix][iy].z = (float) iy/numY * scaleY;
+                
+                float xsum = fNormals[ix][iy].x + fNormals[ixLeft][iy].x + fNormals[ix][iyLeft].x + fNormals[ixLeft][iyLeft].x;
+                float ysum = fNormals[ix][iy].y + fNormals[ixLeft][iy].y + fNormals[ix][iyLeft].y + fNormals[ixLeft][iyLeft].y;
+                float zsum = fNormals[ix][iy].z + fNormals[ixLeft][iy].z + fNormals[ix][iyLeft].z + fNormals[ixLeft][iyLeft].z;
+                
+                vNormals[ix][iy].set(xsum/4, ysum/4, zsum/4);
             }
         }
         
-        for(int j = 0; j < numVertexY-1; j++) {
-            vPositions[numVertexX-1][j].set(vPositions[0][j].x+scaleX, vPositions[0][j].y, vPositions[0][j].z);
-//            vNormals[numVertexX-1][j].set(vNormals[0][j]);
+        for(int iy = 0; iy < numVertexY-1; iy++) {
+            vPositions[numVertexX-1][iy].set(vPositions[0][iy].x+scaleX, vPositions[0][iy].y, vPositions[0][iy].z);
+            vNormals[numVertexX-1][iy].set(vNormals[0][iy]);
         }
-        for(int i = 0; i < numVertexX-1; i++) {
-            vPositions[i][numVertexY-1].set(vPositions[i][0].x, vPositions[i][0].y, vPositions[i][0].z+scaleY);
-//            vNormals[i][numVertexY-1].set(vNormals[i][0]);
+        for(int ix = 0; ix < numVertexX-1; ix++) {
+            vPositions[ix][numVertexY-1].set(vPositions[ix][0].x, vPositions[ix][0].y, vPositions[ix][0].z+scaleY);
+            vNormals[ix][numVertexY-1].set(vNormals[ix][0]);
         }
         vPositions[numVertexX-1][numVertexY-1].set(vPositions[0][0].x+scaleX, vPositions[0][0].y, vPositions[0][0].z+scaleY);
-//        vNormals[numVertexX-1][numVertexY-1].set(vNormals[0][0]);
+        vNormals[numVertexX-1][numVertexY-1].set(vNormals[0][0]);
         
         // final step: bring data model into vertex buffers
         for(int ix = 0; ix < numX; ix++) {
@@ -185,27 +200,51 @@ public class OceanSurface extends Mesh {
                 positionBuffer.put(vPositions[ix][iy].x);
                 positionBuffer.put(vPositions[ix][iy].y);
                 positionBuffer.put(vPositions[ix][iy].z);
+                
+                normalBuffer.put(vNormals[ix][iy].x);
+                normalBuffer.put(vNormals[ix][iy].y);
+                normalBuffer.put(vNormals[ix][iy].z);
 
                 positionBuffer.put(vPositions[ix][iy+1].x);
                 positionBuffer.put(vPositions[ix][iy+1].y);
                 positionBuffer.put(vPositions[ix][iy+1].z);
 
+                normalBuffer.put(vNormals[ix][iy+1].x);
+                normalBuffer.put(vNormals[ix][iy+1].y);
+                normalBuffer.put(vNormals[ix][iy+1].z);
+
                 positionBuffer.put(vPositions[ix+1][iy+1].x);
                 positionBuffer.put(vPositions[ix+1][iy+1].y);
                 positionBuffer.put(vPositions[ix+1][iy+1].z);
-                
+
+                normalBuffer.put(vNormals[ix+1][iy+1].x);
+                normalBuffer.put(vNormals[ix+1][iy+1].y);
+                normalBuffer.put(vNormals[ix+1][iy+1].z);
+
                 // second triangle
                 positionBuffer.put(vPositions[ix][iy].x);
                 positionBuffer.put(vPositions[ix][iy].y);
                 positionBuffer.put(vPositions[ix][iy].z);
 
+                normalBuffer.put(vNormals[ix][iy].x);
+                normalBuffer.put(vNormals[ix][iy].y);
+                normalBuffer.put(vNormals[ix][iy].z);
+
                 positionBuffer.put(vPositions[ix+1][iy+1].x);
                 positionBuffer.put(vPositions[ix+1][iy+1].y);
                 positionBuffer.put(vPositions[ix+1][iy+1].z);
-                
+
+                normalBuffer.put(vNormals[ix+1][iy+1].x);
+                normalBuffer.put(vNormals[ix+1][iy+1].y);
+                normalBuffer.put(vNormals[ix+1][iy+1].z);
+
                 positionBuffer.put(vPositions[ix+1][iy].x);
                 positionBuffer.put(vPositions[ix+1][iy].y);
                 positionBuffer.put(vPositions[ix+1][iy].z);
+
+                normalBuffer.put(vNormals[ix+1][iy].x);
+                normalBuffer.put(vNormals[ix+1][iy].y);
+                normalBuffer.put(vNormals[ix+1][iy].z);
             }
         }
         positionBuffer.rewind();
@@ -222,8 +261,10 @@ public class OceanSurface extends Mesh {
         
         // CPU only data
         vPositions = new Vector3f[numVertexX][numVertexY];
+        vNormals = new Vector3f[numVertexX][numVertexY];
         fHold = new Vector3f[numX][numY];
         mH0 = new Vector2f[numX][numY];
+        fNormals = new Vector3f[numX][numY];
         c = new Vector2f[numX][numY];
         
         positionVBO = new VertexBuffer(Type.Position);
@@ -232,11 +273,42 @@ public class OceanSurface extends Mesh {
         
         normalVBO = new VertexBuffer(Type.Normal);
         normalVBO.setupData(Usage.Stream, 3, Format.Float, normalBuffer);
-//        setBuffer(normalVBO);
+        setBuffer(normalVBO);
         
         setMode(Mode.Triangles);
     }
     
+    private void updateFaceNormals() {
+        float xStep = scaleX/numX;
+        float yStep = scaleY/numY;
+        
+        for(int ix = 0; ix < numX-1; ix++) {
+            for(int iy = 0; iy < numY-1; iy++) {
+                float tax = 0;
+                float tay = (c[ix][iy+1].x-c[ix][iy].x) * waveHeightScale;
+                float taz = yStep;
+                
+                float tbx = xStep;
+                float tby = (c[ix+1][iy].x-c[ix][iy].x) * waveHeightScale;
+                float tbz = 0;
+                
+                // cross product
+                float tcx = tay*tbz - taz*tby;
+                float tcy = taz*tbx - tax*tbz;
+                float tcz = tax*tby - tay*tbx;
+                
+                // set and normalize
+                fNormals[ix][iy].set(tcx, tcy, tcz);
+                fNormals[ix][iy].normalizeLocal();
+            }
+        }
+        
+        for(int ix = 0; ix < numX-1; ix++) { fNormals[ix][numY-1].set(fNormals[ix][0]); }
+        for(int iy = 0; iy < numY-1; iy++) { fNormals[numX-1][iy].set(fNormals[0][iy]); }
+        fNormals[numX-1][numY-1].set(fNormals[0][0]);
+    }
+    
+    // TODO: into interface
     private float calcPhillipsFunction(Vector3f vK) {
         float g = 9.81f;
         
