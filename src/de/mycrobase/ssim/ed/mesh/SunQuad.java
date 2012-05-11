a90b3e90e6e5e33797b422506075591e842dd2b6
cn <cn@mycrobase.de>
2012-05-11 20:30:09 +0200

    Fix wrong number of SunQuad index buffer components
    This value has no effect.

--- src/de/mycrobase/ssim/ed/mesh/SunQuad.java
+++ src/de/mycrobase/ssim/ed/mesh/SunQuad.java
@@ -10,7 +10,7 @@ public class SunQuad extends Mesh {
             new float[] {-size,-size,0, size,-size,0, size, size,0, -size, size,0});
         setBuffer(Type.TexCoord, 2, new float[] {0,0, 1,0, 1,1, 0,1});
         setBuffer(Type.Normal, 3, new float[] {0,0,1, 0,0,1, 0,0,1, 0,0,1});
-        setBuffer(Type.Index, 3, new short[] {0,1,2, 0,2,3});
+        setBuffer(Type.Index, 1, new short[] {0,1,2, 0,2,3});
         updateBound();
     }
 }
