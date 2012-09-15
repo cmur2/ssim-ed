e38a1793ac93a8240319f960ea1f2ee2fe56f7dd
cn <cn@mycrobase.de>
2012-09-15 11:07:34 +0200

    Remove old comments

--- src/ssim/sim/SimConst.java
+++ src/ssim/sim/SimConst.java
@@ -1,21 +1,3 @@
-/*
- * SSim - a realtime Submarine Simulator
- *
- * Copyright (C) 2006-2011  Ch. Nicolai
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 3 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package ssim.sim;
 
 import java.awt.*;
